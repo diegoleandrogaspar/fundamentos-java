d446aeb96ef42dfeeb07eea81ff37ab76a113e5a
diegoleandrogaspar <diegolgaspar000@gmail.com>
2024-08-10 22:18:35 -0300

    exercicio concluido

--- src/estudojava/java/estrutura_sequencial/exercicios/Main6.java
+++ src/estudojava/java/estrutura_sequencial/exercicios/Main6.java
@@ -15,11 +15,25 @@ public class Main6 {
         e) a área do retângulo que tem lados A e B.
          */
 
-
-
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+
+        double triangulo = (a * c) /2;
+        double pi = 3.14159;
+        double circulo = Math.pow(c,2) * pi;
+        double trapezio = ((a + b) * c) /2;
+        double quadrado = b * b;
+        double retangulo = a * b;
+
+        System.out.printf("TRIANGULO: %.3f\n ", triangulo);
+        System.out.printf("CIRCULO: %.3f\n ", circulo);
+        System.out.printf("TRAPEZIO: %.3f\n ", trapezio);
+        System.out.printf("QUADRADO: %.3f\n " , quadrado);
+        System.out.printf("RETANGULO: %.3f\n " , retangulo);
 
         sc.close();
     }
