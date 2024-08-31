49df4276145bca5321d0fbc246493af39a8b9aa0
diegoleandrogaspar <diegolgaspar000@gmail.com>
2024-08-30 23:57:06 -0300

    exerc vetor 1

---
+++ src/estudojava/java/vetores/exercicios/Main01.java
@@ -0,0 +1,31 @@
+package vetores.exercicios;
+
+import java.util.Scanner;
+
+public class Main01 {
+    public static void main(String[] args) {
+        /*
+        Faça um programa que leia um número inteiro positivo N (máximo = 10)
+        e depois N números inteiros e armazene-os em um vetor.
+        Em seguida, mostrar na tela todos os números negativos lidos.
+         */
+
+        Scanner sc = new Scanner(System.in);
+
+
+        int x = sc.nextInt();
+
+        int[] num = new int[x];
+
+        for (int i=0; i< num.length; i++){
+            if (num[i] < 0) {
+                System.out.print(num[i]);
+
+            }
+        }
+
+
+        sc.close();
+
+    }
+}
