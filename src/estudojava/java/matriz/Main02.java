09a6b241d15086a6fb32b45e7db38aa1e290559b
diegoleandrogaspar <diegolgaspar000@gmail.com>
2024-09-06 23:42:22 -0300

    exerc matrizes  02

---
+++ src/estudojava/java/matriz/Main02.java
@@ -0,0 +1,50 @@
+package matriz;
+
+import java.util.Scanner;
+
+public class Main02 {
+    public static void main(String[] args) {
+        /*
+         Fazer um programa para ler dois números inteiros M e N, e depois ler uma matriz de M linhas por N colunas
+         contendo números inteiros, podendo haver repetições. Em seguida, ler um número inteiro X que pertence á matriz.
+         Para cada ocorrência de X, mostrar os valores á esquerda, acima, á direita e abaixo de X, quando houver,
+         conforme exemplo abaixo:
+
+          Exemplo: 3 linhas e 4 colunas
+          3 4
+
+          10  8 15 12
+          21 11 23 8
+          14  5  13 19
+
+           encontrar o número 8
+
+           position 0,1:
+           left: 10
+           Right: 15
+           Down: 11
+           Position 1,3:
+           Left: 23
+           Up: 12
+           Down: 19
+         */
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        int[][] num = new int[x][y];
+
+        for (int i=0; i < num.length; i++){
+            for (int j =0; j <num[y].length; j++){
+                num[i][j] = sc.nextInt();
+            }
+        }
+
+
+
+    }
+}
