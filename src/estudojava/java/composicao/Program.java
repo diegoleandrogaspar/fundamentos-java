95afb3f8f5447cbdc84b39ce6f6f7e664af3ce57
diegoleandrogaspar <diegolgaspar000@gmail.com>
2024-09-07 23:50:52 -0300

    exerc enumeracao e composicao complete 01

---
+++ src/estudojava/java/composicao/Program.java
@@ -0,0 +1,7 @@
+package composicao;
+
+public class Program {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/estudojava/java/enumeracao/entities/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package enumeracao.entities.enums;
+
+public enum OrderStatus {
+    PENDING_PAYMENT,
+    PROCESS,
+    SHIPPED,
+    DELIVERED;
+}
+
