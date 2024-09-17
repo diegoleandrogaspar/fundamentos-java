d16d9011b14402924da27cdfc2d7ae9974755a7c
diegoleandrogaspar <diegolgaspar000@gmail.com>
2024-09-16 23:42:51 -0300

    praticando com function e lambda

--- src/estudojava/java/programacao_funcional_e_lambdas/function/Program.java
+++ src/estudojava/java/programacao_funcional_e_lambdas/function/Program.java
@@ -6,6 +6,7 @@ import programacao_funcional_e_lambdas.function.util.UpperCaseName;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Program {
@@ -23,7 +24,9 @@ public class Program {
         list.add(new Product("Tablet", 350.00));
         list.add(new Product("HD", 80.90));
 
-        List<String> names = list.stream().map(Product::staticUpperCaseName).collect(Collectors.toList());
+        Function<Product, String> func = p -> p.getName().toUpperCase();
+
+        List<String> names = list.stream().map(func).collect(Collectors.toList());
 
         names.forEach(System.out::println);
 
