3d0730304117aae3d54fa172b53a472cfe60c4db
diegoleandrogaspar <diegolgaspar000@gmail.com>
2024-09-23 21:53:12 -0300

    desafio stream concluido

--- src/estudojava/java/programacao_funcional_e_lambdas/stream/Program4.java
+++ src/estudojava/java/programacao_funcional_e_lambdas/stream/Program4.java
@@ -1,7 +1,15 @@
 package programacao_funcional_e_lambdas.stream;
 
+import programacao_funcional_e_lambdas.stream.entities.Funcionario;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Program4 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         /*
         Fazer um programa para ler os dados(nome, email e salário) de funcionários a partir de um arquivo em formato .csv
         Em seguida mostrar, em ordem alfabética, e email dos funcionários cujo salário seja superior a um dado valor fornecido pelo usuário
@@ -23,6 +31,50 @@ public class Program4 {
         bob@gmail.com
         maria@gmail.com
         Soma dos salários das pessoas quando começam com a letra 'M': 4900.00
-         */
+        */
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o caminho do arquivo");
+        String path = sc.nextLine();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            List<Funcionario> list = new ArrayList<>();
+
+            String line = br.readLine();
+            while (line != null){
+                String[] fields = line.split(",");
+                list.add(new Funcionario(fields[0], fields[1], Double.parseDouble(fields[2])));
+                line = br.readLine();
+            }
+
+            Double salarioUsuario = sc.nextDouble();
+
+            List<String> emails = list.stream()
+                    .filter( p -> p.getSalario() > salarioUsuario)
+                    .map(x -> x.getEmail())
+                    .sorted()
+                    .collect(Collectors.toList());
+
+            System.out.println("Email das pessoas quando o salário é maior que aquele da entrada " + salarioUsuario + ":");
+            emails.forEach(System.out::println);
+
+            double salario = list.stream()
+                    .filter( p -> p.getNome().charAt(0) == 'M')
+                    .map(p -> p.getSalario())
+                    .reduce(0.0, (x,y) -> x + y);
+
+            System.out.println(" Somatórios dos salários dos funcionários que começam com a letra 'M' " + String.format("%.2f", salario));
+
+        }
+        catch (IOException ex){
+            System.out.println("Error: " + ex.getMessage());
+        }
+
+
+        sc.close();
+
     }
 }
